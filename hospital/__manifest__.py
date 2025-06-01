ba9e297566ab730f9fa142c8d656e3b8d7a117a1
Tiarintsoa <rihantiana000@gmail.com>
2025-06-01 18:42:27 +0300

    Initial commit

---
+++ hospital/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Hospital",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Tiarintsoa",
+    'description': "An app module to manage an hospital",
+    'category': "Services",
+    'application': True,
+    'license': 'LGPL-3'
+}
