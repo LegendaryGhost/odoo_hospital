214e5c04d663ce4501c2908d9f9d2abb0e3efb7b
Tiarintsoa <rihantiana000@gmail.com>
2025-06-03 11:05:14 +0300

    Disease fix

--- hospital/models/hospital_disease.py
+++ hospital/models/hospital_disease.py
@@ -7,10 +7,3 @@ class HospitalDisease(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     symptom_ids = fields.Many2many('hospital.symptom', string="Symptômes")
-    is_grave = fields.Boolean("Est grave ?", required=True, default=False)
-    needs_medicine = fields.Boolean("A besoin de médicaments ?", required=True, default=True)
-    final_action = fields.Selection([
-        ('go_home_medication', "Rentre avec des médicaments"),
-        ('hospitalize', "Hospitalisé pour le traitement"),
-    ], string="Décision finale", required=True)
-    hospital_days = fields.Integer("Nombre de jours (si hospitalisé)")
