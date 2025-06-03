7941b9a85e67f4a8b2f0ba4e93806ec5ef8581f3
Tiarintsoa <rihantiana000@gmail.com>
2025-06-03 09:29:48 +0300

    Medical Request state

--- hospital/models/hospital_request.py
+++ hospital/models/hospital_request.py
@@ -4,6 +4,15 @@ class HospitalRequest(models.Model):
     _name = 'hospital.request'
     _description = 'Medical Request'
 
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('assigned', 'Assigned'),
+            ('treated', 'Treated'),
+        ],
+        default='new',
+        string='Status',
+    )
     patient_id = fields.Many2one('res.partner', string='Patient', required=True)
     symptom_ids = fields.Many2many('hospital.symptom', string='Symptoms')
     note        = fields.Text(string='Comments')
