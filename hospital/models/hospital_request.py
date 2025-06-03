3e53e5049bb39a47ffdd45520cab3fa6c5928ca7
Tiarintsoa <rihantiana000@gmail.com>
2025-06-03 12:01:58 +0300

    Doctor assignment

--- hospital/models/hospital_request.py
+++ hospital/models/hospital_request.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class HospitalRequest(models.Model):
     _name = 'hospital.request'
@@ -12,9 +12,16 @@ class HospitalRequest(models.Model):
         ],
         default='new',
         string='Status',
+        compute="_compute_state",
     )
     patient_id = fields.Many2one('res.partner', string='Patient', required=True)
     symptom_ids = fields.Many2many('hospital.symptom', string='Symptoms')
     note        = fields.Text(string='Comments')
     nurse_id = fields.Many2one('res.users', string='Nurse', default=lambda self: self.env.user)
     doctor_id = fields.Many2one('res.users', string='Assigned Doctor')
+
+    @api.depends('doctor_id')
+    def _compute_state(self):
+        for record in self:
+            if record.doctor_id:
+                record.state = 'assigned'
